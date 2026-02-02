97fbfa5f1abb377fbc54c892da5472ee078d3868
Bati <no-reply@batijs.dev>
2026-02-02 15:28:17 +0100

    scaffold Vike app with Bati

---
+++ server/entry.ts
@@ -0,0 +1,20 @@
+import { trpcHandler } from "./trpc-handler";
+import { apply, serve } from "@photonjs/express";
+import express from "express";
+
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
+
+export default startApp() as unknown;
+
+function startApp() {
+  const app = express();
+
+  apply(app, [
+    // tRPC route. See https://trpc.io/docs/server/adapters
+    trpcHandler("/api/trpc"),
+  ]);
+
+  return serve(app, {
+    port,
+  });
+}
